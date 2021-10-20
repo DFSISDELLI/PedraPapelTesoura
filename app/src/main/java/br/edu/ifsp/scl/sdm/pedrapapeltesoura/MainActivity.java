650d2d3096f20147e95b606bfd6c82947a8d7ad5
DFSISDELLI <sisdellidafne@gmail.com>
2021-10-20 08:21:35 -0300

    atualizando

--- app/src/main/java/br/edu/ifsp/scl/sdm/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pedrapapeltesoura/MainActivity.java
@@ -3,12 +3,26 @@ package br.edu.ifsp.scl.sdm.pedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import br.edu.ifsp.scl.sdm.pedrapapeltesoura.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(activityMainBinding.getRoot());
+
+
+
+
+    }
+
+    public void onClick(View view) {
     }
 }
